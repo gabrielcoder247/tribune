ea8737d63bb0c43d3f8f7ab219e09130f19cbb80
gabrielcoder <gabrielcoder247@gmail.com>
2018-09-23 23:03:06 +0300

    create template

--- tribune/news/views.py
+++ tribune/news/views.py
@@ -33,6 +33,16 @@ def news_of_day(request):
     return HttpResponse(html)
 
 def past_days_news(request,past_date):
+
+
+    try:
+            # Converts data from the string Url
+            date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+
+    except ValueError:
+            # Raise 404 error when ValueError is thrown
+        raise Http404()
+
     # Converts data from the string Url
     date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
 
