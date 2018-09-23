8f24346d1df1f9cf22073af6f92a86770049cec9
gabrielcoder <gabrielcoder247@gmail.com>
2018-09-23 22:32:27 +0300

    create dynamic content

---
+++ tribune/news/views.py
@@ -0,0 +1,21 @@
+from django.http  import HttpResponse
+import datetime as dt
+
+# Create your views here.
+def welcome(request):
+    return HttpResponse('Welcome to the Moringa Tribune')
+
+
+def news_of_day(request):
+    date = dt.date.today()
+
+    # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
+    day = convert_dates(date)
+    html = f'''
+        <html>
+            <body>
+                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
