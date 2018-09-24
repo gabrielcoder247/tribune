9be8fce36ec1c43203d32a59e75b0692385ac040
gabrielcoder <gabrielcoder247@gmail.com>
2018-09-24 09:51:08 +0300

    install bootstrap3

--- tribune/news/views.py
+++ tribune/news/views.py
@@ -1,6 +1,6 @@
 from django.http  import HttpResponse
 from django.http  import HttpResponse,Http404
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 import datetime as dt
 
 # Create your views here.
@@ -45,16 +45,9 @@ def past_days_news(request,past_date):
     except ValueError:
             # Raise 404 error when ValueError is thrown
         raise Http404()
+        assert False
 
-    # Converts data from the string Url
-    date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+    if date == dt.date.today():
+        return redirect(news_today)
 
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
+    return render(request, 'all-news/past-news.html', {"date": date})    
