3f5bde799e3050d9c848c80c4b7002a545200f03
gabrielcoder <gabrielcoder247@gmail.com>
2018-09-24 03:02:36 +0300

    create base template

--- tribune/news/views.py
+++ tribune/news/views.py
@@ -1,10 +1,13 @@
 from django.http  import HttpResponse
 from django.http  import HttpResponse,Http404
+from django.shortcuts import render
 import datetime as dt
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to the Moringa Tribune')
+
+    return render(request, 'welcome.html')
+    # return HttpResponse('Welcome to the Moringa Tribune')
 
 def convert_dates(dates):
 
