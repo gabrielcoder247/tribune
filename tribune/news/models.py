d6fd5a7d12262fd5b17c9fff050bb76af6032d31
gabrielcoder <gabrielcoder247@gmail.com>
2018-09-25 15:58:20 +0300

    create superuser for admin

--- tribune/news/models.py
+++ tribune/news/models.py
@@ -7,8 +7,12 @@ class Editor(models.Model):
     last_name = models.CharField(max_length = 30)
     email = models.EmailField()
 
+    def save_editor(self):
+        self.save()
+
     def __str__(self):
         return self.first_name
+        
 
     class meta:
         ordering = ['first_name'] 
@@ -22,9 +26,17 @@ class tags(models.Model):
 
 class Article(models.Model):
     title = models.CharField(max_length = 60)
-    post = models.Textfield()
+    post = models.TextField()
     editor = models.ForeignKey(Editor)
     tags = models.ManyToManyField(tags)
     pub_date = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.title
+
+
+    def save_article(self):
+        self.save()
 
 
--- tribune/news/tests.py
+++ tribune/news/tests.py
@@ -1,3 +1,41 @@
 from django.test import TestCase
+from . models import Editor,Article,tags
 
 # Create your tests here.
+
+class EditorTestCase(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.gabriel = Editor(first_name = 'gabriel', last_name = 'nwachukwu', email = 'gabrielcoder247@gmail.com')
+
+    # Test instance    
+    def test_instance(self):
+        self.assertTrue(isinstance(self.gabriel,Editor))
+
+    def test_save_method(self):
+        self.gabriel.save_editor()
+        editors = Editor.objects.all()
+        self.assertTrue(len(editors) > 0)
+
+
+
+class ArticleTestCase(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.new_article = Article( 
+        title = 'arm robber caught in abuja',
+        editor_id = 4,
+        post = 'five armed men were caught last night with sophisticated weapons',
+        pub_date = 2018-10-25)
+
+    # Test instance    
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_article,Article))
+
+    def test_save_method(self):
+        self.new_article.save_article()
+        articles = Article.objects.all()
+        self.assertTrue(len(articles) > 0)        
+
