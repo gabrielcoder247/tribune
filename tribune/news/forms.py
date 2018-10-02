038a7235dc223a0449928f75ae66e5f5a3b59737
gabrielcoder <gabrielcoder247@gmail.com>
2018-10-02 14:10:03 +0300

    create views function for new_article.html

--- tribune/news/forms.py
+++ tribune/news/forms.py
@@ -6,7 +6,7 @@ class NewsLetterForm(forms.Form):
     your_name = forms.CharField(label='First Name', max_length=30)
     email = forms.EmailField(label='Email')
 
-class NewsArticleForm(forms.modelForm):
+class NewArticleForm(forms.modelForm):
     class Meta:
         model =Article
         exclude = ['editor', 'pub_date']
--- tribune/news/views.py
+++ tribune/news/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse, Http404,HttpResponseRedirect
-from .forms import NewsLetterForm
+from .forms import NewsLetterForm,NewLetterForm
 from django.contrib.auth.decorators import login_required
 from .email import send_welcome_email
 from .models import Article
@@ -58,6 +58,22 @@ def article(request,article_id):
         raise Http404()
 
     return render(request, "all-news/article.html",{ "article":article}) 
+
+@login_required(login_url='/accounts/login/')
+def new_article(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewArticleForm(request.POST, request.FILES)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.editor = current_user
+            article.save()
+        return redirect('newsToday')
+    else:
+        form = NewArticleForm()
+    return render(request,'new_article.html', {"form":form})        
+
+
 
 def search_results(request): 
       
